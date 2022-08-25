8f68eb3f4a475b795360ff61989e937f82f8ddd9
Deepak <deepak@localhost>
2022-08-26 00:20:04 +0530

    add payment gateway

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 // DataBase Connection
-mongoose.connect( process.env.DATABASE, {
+mongoose.connect( process.env.DATABASELocal, {
     useNewUrlParser:true,
-    useUnifiedTopology:true,
-    useCreateIndex:true
+    useUnifiedTopology:true
+    // useCreateIndex:true
 }).then(() => {
     console.log("[ User Database Connected... ]");
 }).catch((e) => {
-    console.log("[ User Database Connection Error! ]");
+    console.log(e,"[ User Database Connection Error! ]");
 })
